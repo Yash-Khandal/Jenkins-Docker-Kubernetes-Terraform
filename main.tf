24232f8203cab7f15741b2ccf70dd740efe121af
yash khandal <135641850+Yash-Khandal@users.noreply.github.com>
2025-04-15 11:51:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,52 +1,14 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.0.0"
-    }
-  }
-  required_version = ">=1.0.0"
-}
-
 provider "azurerm" {
-  subscription_id = "6c1e198f-37fe-4942-b348-c597e7bef44b"
-  tenant_id       = "341f4047-ffad-4c4a-a0e7-b86c7963832b"
-  client_id       = "4cc4e731-d820-406c-831c-998643733aa3"
-  client_secret   = "C6t8Q~VxVt-35pzGG1RhWTmhCLoHIWiP6rh6capS"
   features {}
 }
 
-variable "location" {
-  default = "East US"
-}
-
-variable "resource_group_name" {
-  default = "myResourceGroup"
-}
-
-variable "acr_name" {
-  default = "acryash20240415"
-}
-
-variable "aks_cluster_name" {
-  default = "myAKSCluster"
-}
-
-variable "dns_prefix" {
-  default = "myakscluster"
-}
-
-variable "node_count" {
-  default = 2
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "myResourceGroup"
+  location = "East US"
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
+  name                = "acryash20240415"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
@@ -54,14 +16,14 @@ resource "azurerm_container_registry" "acr" {
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_cluster_name
+  name                = "myAKSCluster"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = var.dns_prefix
+  dns_prefix          = "myakscluster"
 
   default_node_pool {
     name       = "default"
-    node_count = var.node_count
+    node_count = 1
     vm_size    = "Standard_DS2_v2"
   }
 
@@ -69,34 +31,5 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 
-  network_profile {
-    network_plugin    = "azure"
-    load_balancer_sku = "standard"
-  }
-
-  tags = {
-    Environment = "Development"
-  }
-}
-
-resource "azurerm_role_assignment" "aks_acr_pull" {
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name = "AcrPull"
-  scope                = azurerm_container_registry.acr.id
-
-  depends_on = [
-    azurerm_kubernetes_cluster.aks
-  ]
-}
-
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
-
-output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks.name
+  depends_on = [azurerm_container_registry.acr]
 }
